2500a1f91977712a05ef77c07555a152df9d8c9c
yagiin <yagi@inet-square.com>
2026-06-14 22:35:03 +0900

    fix(web): update operator name

--- web/src/components/top/team-mirai.tsx
+++ web/src/components/top/team-mirai.tsx
@@ -5,7 +5,7 @@ export function TeamMirai() {
         {/* ヘッダー */}
         <div className="flex flex-col gap-4">
           <h2 className="text-xl font-bold text-black">
-            やぎーん（草津市議会議員）
+            八木よしと（草津市議会議員）
           </h2>
           <p className="text-sm font-bold text-primary-accent">
             このサイトについて
@@ -16,7 +16,7 @@ export function TeamMirai() {
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-3">
             <p className="text-[15px] leading-[28px] text-black">
-              草津市議会議員のやぎーんが、チームみらいのOSS「みらい議会」をForkして作成した草津市議会版サイトです。これは政党チームみらいが運営しているものではありません。
+              草津市議会議員の八木よしとが、チームみらいのOSS「みらい議会」をForkして作成した草津市議会版サイトです。これは政党チームみらいが運営しているものではありません。
             </p>
           </div>
         </div>
